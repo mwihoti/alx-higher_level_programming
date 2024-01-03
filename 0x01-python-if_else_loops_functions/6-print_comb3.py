664ac898afed9ad896534a903c18a39dba5f065e
mwihoti <danielmwihoti@gmail.com>
2024-01-02 19:10:02 -0800

    g

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for num in range(0, 10):
     for digit in range(num + 1, 10):
-        if num == 7 and digit == 8:
+        if num == 8 and digit == 9:
             print("{}{}".format(num, digit))
         else:
             print("{}{}".format(num, digit), end=", ")
