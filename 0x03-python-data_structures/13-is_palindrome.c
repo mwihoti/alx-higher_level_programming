a0555a9b099db82e51d1af1af4596a1ab78e71b4
Daniel Edward Mwihoti <86232216+mwihoti@users.noreply.github.com>
2024-01-09 00:13:58 +0300

    Update 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,72 @@
 #include "lists.h"
+
 /**
- * palindrome - check for palindrome
- * @l: l
- * @r: r
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: 1 palindrome, 0 not palindrome
+ * Return: pointer to the first node in the new list
  */
-int palindrome(listint_t **l, listint_t *r)
+void reverse_listint(listint_t **head)
 {
-	int res;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	if (r != NULL)
+	while (current)
 	{
-		res = palindrome(l, r->next);
-		if (res != 0)
-		{
-			res = (r->n == (*l)->n);
-			*l = (*l)->next;
-			return (res);
-		}
-		return (0);
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	return (1);
-}
-/**
- * is_palindrome - checks if list is a palindrome
- * @head: head of linked_list
- *
- * Return: 1 palindrome, 0 mot palindrome
- */
 
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL)
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (1)
 	{
-		return (0);
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	return (palindrome(head, *head));
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
