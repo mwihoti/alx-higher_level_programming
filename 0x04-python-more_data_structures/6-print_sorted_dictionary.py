4087a829a5a40ade996a0ac6a600dd21e1ce81e7
mwihoti <danielmwihoti@gmail.com>
2024-01-10 07:55:30 -0800

    sorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
+    for m in sorted(a_dictionary.keys()):
+        print("{}: {}".format(m, a_dictionary.get(m)))
