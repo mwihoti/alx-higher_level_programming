a015de9678cd14dabcd4e8850d7933d842ff47da
mwihoti <danielmwihoti@gmail.com>
2024-01-23 11:50:46 -0800

    6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
 
     def my_print(self):
         """prints in stdout the square with the character #"""
-        if self.__Size == 0:
+        if self.__size == 0:
             print("")
             return
 
