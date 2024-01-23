0c9a440547611ee3f8fe991e54070ca52f0048d0
mwihoti <danielmwihoti@gmail.com>
2024-01-23 11:56:46 -0800

    last

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 
 class Square:
-    def __init__(self, size=0, position=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializing atrtribute
         args:
             position - square position
