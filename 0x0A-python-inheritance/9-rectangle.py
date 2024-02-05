669a2247b5f027aea68cb4adc07625f06abd3c1c
mwihoti <danielmwihoti@gmail.com>
2024-02-05 14:56:17 -0800

    full Rectangle that returns string

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+"""
+Inherits from BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle which inherits
+    from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        initializes the method
+
+        Args:
+            height: "height"
+            width: "width"
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        method to get area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        method that returns string
+        """
+        return "[Rectaangle] {}/{}".format(self.__width, self.__height)
