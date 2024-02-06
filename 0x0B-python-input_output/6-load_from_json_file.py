11a2a81a695397f9c679d99e0b6e424d17df3179
mwihoti <danielmwihoti@gmail.com>
2024-02-06 06:56:43 -0800

    creates object from Json fie

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines function that creates an object from JSON File"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates object from a JSON file
+    """
+    with open(filename) jfile:
+        return json.load(jfile")
