4cf3ed3d525d2efc3de92d598dff61c2c7a8ff35
mwihoti <danielmwihoti@gmail.com>
2024-02-05 23:08:38 -0800

    removed module1

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-# BaseGeometry class is imported from another module
+"""BaseGeometry class is imported from another module"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-"""
-==============================
-Class Rectangle
-===================================
-"""
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
+        """Initializes Rectangle
+        Args:
+            width(int): width
+            height(int): height
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
