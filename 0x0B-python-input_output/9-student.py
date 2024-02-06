41181459afa586c7cddec54b40f15614e9b8e487
mwihoti <danielmwihoti@gmail.com>
2024-02-06 07:35:07 -0800

    students to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines class student that defines a student"""
+
+
+class Student:
+    """class student """
+    def __init__(self, first_name, last_name, age):
+        """
+        initializes student
+
+        Args:
+            first_name: first name of student
+            last_name: last name of the student
+            age: student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """ dictionary representation of a student"""
+            return self.__dict__
