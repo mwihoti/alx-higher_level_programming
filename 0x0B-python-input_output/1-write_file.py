da20da48f25a937045976bdf3d9d782acc4a13fe
mwihoti <danielmwihoti@gmail.com>
2024-02-06 06:23:21 -0800

    Write to text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines a function writes a string to text file
+"""
+def write_file(filename="", text=""):
+    """
+    writes string to a text file
+    """
+    with open(filename, "w", encoding="utf-8") as t:
+        return t.write(text)
