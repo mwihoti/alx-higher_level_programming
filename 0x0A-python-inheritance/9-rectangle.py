5d0e2a05adffd3598cd7c8fc862d8cedd2a5cbe2
mwihoti <danielmwihoti@gmail.com>
2024-02-05 23:10:56 -0800

    9-rectangle correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
 """
 Inherits from BaseGeometry
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
