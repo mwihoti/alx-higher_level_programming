09150912cf946edbd75b4668015cb0cef2473012
mwihoti <danielmwihoti@gmail.com>
2024-02-06 10:09:27 -0800

    json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines class student """
+
+
+class Student:
+    """
+    represents class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name(str): student first name
+            last_name(str): student last name
+            age (int): student age
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves dictionary representation of student
+        """
+        if (type(attrs) == list and
+                all(type(elem) == str for elem in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
