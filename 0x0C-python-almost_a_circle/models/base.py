82b64afd309ec0bbce64f96a8c27e3d45290b014
mwihoti <danielmwihoti@gmail.com>
2024-02-11 08:52:21 -0800

    Dictionary to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,27 @@ class Base:
             else:
                 list_dict = [k.to_dictionary() for k in list_objs]
                 jsfile.write(Base.to_json_string(list_dict))
+    
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+
+        if dictionary and dictionary != ():
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
