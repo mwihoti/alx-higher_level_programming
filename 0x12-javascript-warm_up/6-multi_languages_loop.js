8c636caf9e3c95bc0d0773d3359bc74dbb79d39d
mwihoti <danielmwihoti@gmail.com>
2024-04-08 10:06:20 -0700

    output correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of languages) {
   console.log(i);
 }
