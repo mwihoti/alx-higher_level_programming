574a37032a6de1f69bd19496fde6d667d09675f1
mwihoti <danielmwihoti@gmail.com>
2024-04-12 10:10:06 -0700

    changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Add State objects to the database"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    lous = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
