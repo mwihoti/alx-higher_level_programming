80f410e04cb6aa9ac760906c1779af23d3b96b2b
mwihoti <danielmwihoti@gmail.com>
2024-04-11 22:30:56 -0700

    lists all states from the database Requirement correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost" user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    [print(state) for state in cur.fetchall()]
+    for state in cur.fetchall():
+        print(state)
     cur.close()
     db.close()
