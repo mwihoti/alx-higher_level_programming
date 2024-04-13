6075a4847f6285f46e341fac3b99aa4a728b8b78
mwihoti <danielmwihoti@gmail.com>
2024-04-12 18:43:18 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Deletes the State object "Louisiana" from the database hbtn_0e_6_usa"""
 from sqlalchemy import create_engine
+import sys
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
