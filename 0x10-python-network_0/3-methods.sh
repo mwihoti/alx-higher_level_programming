d58b9775b0d77072085fcece7011698758ac59d4
mwihoti <danielmwihoti@gmail.com>
2024-04-25 05:30:29 -0700

    3-methods.sh & 4-header.sh corrections

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS $1 | grep "allow" | cut -d " " -f 2-
+# Bash script that takes in a URL and displays all HTTP methods
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
