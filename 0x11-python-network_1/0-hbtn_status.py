5fd1982a633649650168d62f0d8526a84b8cc8bc
mwihoti <danielmwihoti@gmail.com>
2024-04-26 01:32:43 -0700

    Python script that fetches url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
