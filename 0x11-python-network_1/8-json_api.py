790b37819ae3f3df43f53fd75508a1a7acfdeb22
mwihoti <danielmwihoti@gmail.com>
2024-04-26 06:51:12 -0700

    search Api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,4 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """
 script that takes in a letter and sends a POST request to http://0.0.0.0:5000/\
         search_user with the letter as a parameter.
@@ -8,4 +8,15 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else  sys.argv[1]
+    l = {"q" : letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", l)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
