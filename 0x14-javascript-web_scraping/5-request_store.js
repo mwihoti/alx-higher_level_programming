f4a039e85ccc6df5bbcaa88bee3e19b815e47edb
mwihoti <danielmwihoti@gmail.com>
2024-05-22 05:23:59 -0700

    Correction of 5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const req = require('request');
-const fs = require('fs')
+const fs = require('fs');
 
-req(process.argv[2], function(err, res, body) {
+req(process.argv[2], function (res, body) {
   fs.writeFile(process.argv[3], body, 'utf8', function (err) {
     if (err) {
       console.log(err);
