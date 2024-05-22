f17001f94f32aff6d392e1f8127e6e375d59d18b
mwihoti <danielmwihoti@gmail.com>
2024-05-22 06:10:56 -0700

    How many tasks completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,20 @@
 #!/usr/bin/node
 
-const 
+const request = require('request');
+
+request(process.argv[2], (error, res, body) => {
+  const numTasks = {};
+  if (error) {
+    console.log(error);
+  }
+  const json = JSON.parse(body);
+  json.forEach(task => {
+    if (task.completed) {
+      if (!numTasks[task.userId]) {
+        numTasks[task.userId] = 0;
+      }
+      numTasks[task.userId]++;
+    }
+  });
+  console.log(numTasks);
+});
