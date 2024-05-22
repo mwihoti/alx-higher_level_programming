910b1c87d383adb82984af1363f2196bf73a3849
mwihoti <danielmwihoti@gmail.com>
2024-05-22 05:44:02 -0700

    Correction of 5. Loripsum semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,9 +4,13 @@ const req = require('request');
 const fs = require('fs');
 
 req(process.argv[2], function (error, res, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+  if (error == null) {
+    fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    console.log(error);
+  }
 });
